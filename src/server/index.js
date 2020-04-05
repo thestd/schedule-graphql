964b725f395a64876fed4de60cc015641692cfcd
Rostislav Ivankiv <bbwgd77@gmail.com>
2020-04-05 17:26:09 +0300

    Dockerize an application

--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,10 @@ const { ApolloServer } = require('apollo-server');
 const { typeDefs } = require('./graphql/schema')
 const { resolvers } = require('./graphql/resolvers')
 
+const PORT = process.env.PORT || 3000;
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
+server.listen(PORT).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
 });
