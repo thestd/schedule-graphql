9c92022e2450d3cc814c90d27f66f693e2b42edf
Rostislav Ivankiv <bbwgd77@gmail.com>
2020-04-05 03:25:25 +0300

    Add valid schema

--- src/server/graphql/schema.js
+++ src/server/graphql/schema.js
@@ -1,7 +1,33 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
+  type ScheduleItem {
+    number: String
+    time_bounds: String
+    info: String
+  }
+
+  type Schedule {
+    date: String
+    day: String
+    items: [ScheduleItem]
+  }
+  
+  type ScheduleResource {
+    group: String
+    schedule: [Schedule]
+  }
+  
+  type Faculty {
+    name: String
+    code: Int
+  }
+
   type Query {
+    scheduleResource(group: String, teacher: String, date_from: String, date_to: String): ScheduleResource!
+    groups(query: String): [String]
+    teachers(query: String, faculty: Int): [String]
+    faculties: [Faculty]
   }
 `;
 
